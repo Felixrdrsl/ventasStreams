759a9aeaaf421d7924e16d8d1d6d4cab797ba580
felixrdrsl <felixrdrsl@gmail.com>
2026-04-14 17:00:13 +0200

    nuevo

--- src/main/java/services/ConsultasVenta.java
+++ src/main/java/services/ConsultasVenta.java
@@ -3,7 +3,9 @@ package services;
 import lombok.Data;
 import models.Venta;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Data
 public class ConsultasVenta {
@@ -21,6 +23,49 @@ public class ConsultasVenta {
     public List<Venta> getVentasMayor100(){
         return ventas.stream()
                 .filter(venta -> venta.getTotalLinea() > 100)
+                .toList();
+    }
+
+    /**
+     * Develve la categoria Electronics
+     * @return
+     */
+    public List<Venta> getVentasByCategoriaElectronica(){
+        return ventas.stream()
+                .filter(v -> v.getCategoria().equals("Electronics"))
+                .toList();
+    }
+
+    /**
+     * Devuelve los productos vendidos ordenador por nombre. Sin repetidos.
+     * @return
+     */
+    public List<String> getProductosVendidosOrder(){
+        return ventas.stream()
+                .map(Venta::getProducto)
+                .distinct()
+                .sorted()
+                .toList();
+    }
+
+    /**
+     * Devuelve la primera Venta en España
+     * @return
+     */
+    public Optional<Venta> getPrimeraVentaSpain(){
+        return ventas.stream()
+                .filter(v -> v.getPais().equals("Spain"))
+                .findFirst();
+    }
+
+    /**
+     * Devuelve el Top10 ventas por importe total
+     * @return
+     */
+    public List<Venta> getTop10VentasPorImporteTotal(){
+        return ventas.stream()
+                .sorted(Comparator.comparing(Venta::getTotalLinea).reversed())
+                .limit(10)
                 .toList();
     }
 
